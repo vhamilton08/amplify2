c11bade518c23b962227b1435f22e967c804e096
vhamilton08 <vhamilton08@gmail.com>
2020-09-18 16:05:39 -0500

    todo app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { listTodos } from './graphql/queries';
+import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
+import { API } from 'aws-amplify';
+
+const initialFormState = { name: '', description: '' }
 
 function App() {
-  return (
+  const [notes, setNotes] = useState([])
+  const [formData, setFormData] = useState(initialFormState)
+
+  useEffect(() => {
+    fetchNotes() 
+  }, [])
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listTodos })
+    setNotes(apiData.data.listTodos.items)
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({query: createTodoMutation, variables: {input:formData} })
+    setNotes([...notes, formData])
+    setFormData(initialFormState)
+  }
+
+  async function deleteNote({id}) {
+    const newNotesArray = notes.filter(note => note.id !== id)
+    setNotes(newNotesArray)
+    await API.graphql({ query: deleteTodoMutation, variables: {input: {id} }})
+}
+
+ return (
     <div className="App">
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>We now have Auth!</h1>
-      </header>
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            </div>
+          ))
+        }
+      </div>
       <AmplifySignOut />
     </div>
   );
 }
+
 
 export default withAuthenticator(App);
